bf4246a655786680727f4fc25e4f5461bae69dce
Steve Krulewitz <steve.krulewitz@airbnb.com>
2020-07-21 17:00:15 -0700

    Fix elasticsearch

--- common/elasticsearch/client.go
+++ common/elasticsearch/client.go
@@ -91,6 +91,8 @@ func NewClient(config *Config) (Client, error) {
 		elastic.SetURL(config.URL.String()),
 		elastic.SetRetrier(elastic.NewBackoffRetrier(elastic.NewExponentialBackoff(128*time.Millisecond, 513*time.Millisecond))),
 		elastic.SetDecoder(&elastic.NumberDecoder{}), // critical to ensure decode of int64 won't lose precise
+		elastic.SetSniff(false),
+		elastic.SetHealthcheck(false),
 	)
 	if err != nil {
 		return nil, err
